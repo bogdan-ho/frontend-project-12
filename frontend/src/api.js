e5f5504e6238dced76c9a0beb9db5a664942eaba
Bogdan <homoretsky@gmail.com>
2022-12-02 15:27:49 +0500

    Add socket api for channels adding & rendering

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,6 +2,7 @@ import { io } from 'socket.io-client';
 import { useMemo } from 'react';
 // import { actions as channelsActions } from './slices/channelsSlice';
 import { actions as messagesActions } from './slices/messagesSlice';
+import { actions as channelActions } from './slices/channelsSlice';
 import { SocketContext } from './contexts/index';
 
 /*
@@ -24,13 +25,14 @@ socket.on('connect', () => {
   console.log(socket.connected); // true
 });
 
+// messages
 const subscribeOnMessages = (dispatch) => {
   socket.on('newMessage', (payload) => {
     console.log('payload', payload); // => { body: "new message", channelId: 7, id: 8, username: "admin" }
     dispatch(messagesActions.addMessage(payload));
   });
 };
-const unsubscribeFromMessages = () => {
+const unsubscribeMessages = () => {
   socket.off('newMessage');
   console.log('unsubscribe from messages');
 };
@@ -41,11 +43,46 @@ const sendMessage = (value, currentChannelId, username) => {
   socket.emit('newMessage', { body: [value.body], channelId: currentChannelId, username });
 };
 
+// channels
+const subscribeNewChannel = (dispatch) => {
+  socket.on('newChannel', (payload) => {
+    console.log('newChannel payload', payload); // { id: 6, name: "new channel", removable: true }
+    dispatch(channelActions.addChannel(payload));
+  });
+};
+
+const unsubscribeNewChannel = () => {
+  socket.off('newChannel');
+  console.log('unsubscribe from newChannel');
+};
+
+const subscribeRemoveChannel = (dispatch) => {
+  socket.on('removeChannel', (payload) => {
+    console.log('removeChannel payload', payload); // { id: 6, name: "new channel", removable: true }
+    dispatch(channelActions.removeChannel(payload));
+  });
+};
+
+const unsubscribeRemoveChannel = () => {
+  socket.off('removeChannel');
+  console.log('unsubscribe from RemoveChannel');
+};
+
+const emitNewChannel = (name) => {
+  console.log('emitNewChannel name', name);
+  socket.emit('newChannel', { name });
+};
+
 const SocketProvider = ({ children }) => (
   <SocketContext.Provider value={useMemo(() => ({
     subscribeOnMessages,
-    unsubscribeFromMessages,
+    unsubscribeMessages,
     sendMessage,
+    subscribeNewChannel,
+    unsubscribeNewChannel,
+    subscribeRemoveChannel,
+    unsubscribeRemoveChannel,
+    emitNewChannel,
   }), [])}
   >
     {children}

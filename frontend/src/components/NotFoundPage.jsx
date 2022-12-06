fd948bc540ad40d41b51bbc1d2473d0825f5855e
Bogdan <homoretsky@gmail.com>
2022-12-06 13:03:18 +0500

    Add 404 page

--- frontend/src/components/NotFoundPage.jsx
+++ frontend/src/components/NotFoundPage.jsx
@@ -1,3 +1,18 @@
-const NotFoundPage = () => <div>This is NotFoundPage</div>;
+import { useTranslation } from 'react-i18next';
+import image from '../assets/404.svg';
+
+const NotFoundPage = () => {
+  const { t } = useTranslation();
+  return (
+    <div className="text-center">
+      <img alt="Страница не найдена" className="img-fluid h-25" src={image} />
+      <h1 className="h4 text-muted">{t('notFoundPage.title')}</h1>
+      <p className="text-muted">
+        {t('notFoundPage.text')}
+        <a href="/">{t('notFoundPage.link')}</a>
+      </p>
+    </div>
+  );
+};
 
 export default NotFoundPage;

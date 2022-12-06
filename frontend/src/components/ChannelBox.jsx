cf5915c1f9c5c6f0f8a759b886a722ed111c5a1b
Bogdan <homoretsky@gmail.com>
2022-12-06 13:15:08 +0500

    Update useEffect dependencies

--- frontend/src/components/ChannelBox.jsx
+++ frontend/src/components/ChannelBox.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect } from 'react';
 import { ButtonGroup, Dropdown, Button } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';

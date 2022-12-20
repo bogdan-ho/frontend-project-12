9f4699e2318869af7ed48e8707c4450be10ed662
Bogdan <homoretsky@gmail.com>
2022-12-20 14:30:23 +0500

    update styles

--- frontend/src/pages/App.jsx
+++ frontend/src/pages/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import {
   BrowserRouter, Routes, Route, Link,
 } from 'react-router-dom';
@@ -17,56 +17,42 @@ import PrivateRoute from './helpers/PrivateRoute';
 import AuthButton from './helpers/AuthButton';
 import RollbarProvider from './helpers/RollbarProvider';
 
-const App = () => {
-  useEffect(() => {
-    document.documentElement.classList.add('h-100');
-    document.getElementById('root').classList.add('h-100');
-    document.body.classList.add('h-100', 'bg-light');
+const App = () => (
+  <RollbarProvider>
+    <SocketProvider>
+      <AuthProvider>
+        <div className="h-100">
+          <div className="h-100" id="chat">
+            <div className="d-flex flex-column h-100">
+              <BrowserRouter>
+                <Navbar className="shadow-sm bg-white">
+                  <Container>
+                    <Navbar.Brand as={Link} to="/">Hexlet Chat</Navbar.Brand>
+                    <AuthButton />
+                  </Container>
+                </Navbar>
 
-    return () => {
-      document.documentElement.classList.remove('h-100');
-      document.getElementById('root').classList.remove('h-100');
-      document.body.classList.remove('h-100', 'bg-light');
-    };
-  }, []);
-
-  return (
-    <RollbarProvider>
-      <SocketProvider>
-        <AuthProvider>
-          <div className="h-100">
-            <div className="h-100" id="chat">
-              <div className="d-flex flex-column h-100">
-                <BrowserRouter>
-                  <Navbar className="shadow-sm bg-white">
-                    <Container>
-                      <Navbar.Brand as={Link} to="/">Hexlet Chat</Navbar.Brand>
-                      <AuthButton />
-                    </Container>
-                  </Navbar>
-
-                  <Routes>
-                    <Route
-                      path="/"
-                      element={(
-                        <PrivateRoute>
-                          <ChatPage />
-                        </PrivateRoute>
-                      )}
-                    />
-                    <Route path="/login" element={<LoginPage />} />
-                    <Route path="/signup" element={<SignUpPage />} />
-                    <Route path="*" element={<NotFoundPage />} />
-                  </Routes>
-                </BrowserRouter>
-              </div>
-              <ToastContainer />
+                <Routes>
+                  <Route
+                    path="/"
+                    element={(
+                      <PrivateRoute>
+                        <ChatPage />
+                      </PrivateRoute>
+                    )}
+                  />
+                  <Route path="/login" element={<LoginPage />} />
+                  <Route path="/signup" element={<SignUpPage />} />
+                  <Route path="*" element={<NotFoundPage />} />
+                </Routes>
+              </BrowserRouter>
             </div>
+            <ToastContainer />
           </div>
-        </AuthProvider>
-      </SocketProvider>
-    </RollbarProvider>
-  );
-};
+        </div>
+      </AuthProvider>
+    </SocketProvider>
+  </RollbarProvider>
+);
 
 export default App;
